1d5ec9c46122b34ee9b8012515458f1c81dda719
Paul Annesley <paul@annesley.cc>
2011-04-29 23:24:11 +1000

    Top level flip.rb loads all files in lib/flip/*.rb

--- lib/flip.rb
+++ lib/flip.rb
@@ -4,5 +4,16 @@
   core_ext/hash/reverse_merge
 }.each { |name| require "active_support/#{name}" }
 
+%w{
+  abstract_strategy
+  controller_filters
+  cookie_strategy
+  database_strategy
+  declarable
+  declaration_strategy
+  definition
+  feature_set
+}.each { |name| require "flip/#{name}" }
+
 module Flip
 end

ed25c7e7e64bb513188130da3a98e56be6a92be5
Paul Annesley <paul@annesley.cc>
2011-05-02 22:12:11 +1000

    Basic spec coverage for Flip::Definition.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,5 +1,19 @@
+require "spec_helper"
+
 describe Flip::Definition do
 
-  pending
+  subject { Flip::Definition.new :the_key, description: "The description" }
+
+  [:key, :name, :to_s].each do |method|
+    its(method) { should == :the_key }
+  end
+
+  its(:description) { should == "The description" }
+  its(:options) { should == { description: "The description" } }
+
+  context "without description specified" do
+    subject { Flip::Definition.new :the_key }
+    its(:description) { should == "The key." }
+  end
 
 end

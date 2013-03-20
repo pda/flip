7e78567bdbab318f51fb7659e452eeca918916cd
David Ed Mellum <david@edmellum.com>
2013-03-20 14:45:36 +0100

    Rename initializer generator to plural features.rb instead of feature.rb

--- lib/generators/flip/initializer/initializer_generator.rb
+++ lib/generators/flip/initializer/initializer_generator.rb
@@ -2,7 +2,7 @@ class Flip::InitializerGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def create_initializer_file
-    copy_file "feature.rb", "config/initializers/feature.rb"
+    copy_file "features.rb", "config/initializers/features.rb"
   end
 
 end

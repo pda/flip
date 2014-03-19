1fb18b1b43af5690b75cbb96aef7b37b20c5f30f
Paul Annesley <paul@annesley.cc>
2014-03-19 11:33:19 -0700

    Revert "DatabaseStrategy#switch! uses `update_attributes!`."
    This reverts commit 511ee075d7a948f3ffecb91ace08244f86b168ef.
    
    @bsharpe commented on 511ee07:
    
    > Unfortunately, I'm unclear how this passed the test as you cannot call
    > .update_attributes! on a record that hasn't been saved yet. This fails in
    > production with features that do not have records created for them yet.
    >
    > My original patch was to fix just this issue.
    >
    > Using the more procedural approach guarantees that you can set the param and
    > that the record is saved.

--- lib/flip/database_strategy.rb
+++ lib/flip/database_strategy.rb
@@ -23,9 +23,9 @@ module Flip
     end
 
     def switch! key, enable
-      @klass.where(key: key.to_s).first_or_initialize.update_attributes!(
-        enabled: enable
-      )
+      record = @klass.where(key: key.to_s).first_or_initialize
+      record.enabled = enable
+      record.save!
     end
 
     def delete! key

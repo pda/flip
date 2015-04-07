5e6ed9116389cadeaa13ffb07749e383eb1b2d94
Juan F. Pérez <j.guitar@gmail.com>
2015-04-07 10:51:40 +0200

    isolated engine is not a requirement to use path helpers, but name

--- lib/flip/engine.rb
+++ lib/flip/engine.rb
@@ -1,9 +1,8 @@
 module Flip
   class Engine < ::Rails::Engine
-
+    engine_name 'flip'
     initializer "flip.blarg" do
       ActionController::Base.send(:include, Flip::CookieStrategy::Loader)
     end
-
   end
 end
